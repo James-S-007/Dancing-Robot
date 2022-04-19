9071d6aac51acc75bef14cbcab71bbaeac3b7043
jkaranouhs <78691589+jkaranouhs@users.noreply.github.com>
2022-04-19 15:53:48 -0400

    Update mbed_comms.py

--- rpi/mbed_comms.py
+++ rpi/mbed_comms.py
@@ -1,20 +1,24 @@
 #https://www.engineersgarage.com/articles-raspberry-pi-serial-communication-uart-protocol-ttl-port-usb-serial-boards/
 import time
 from time import sleep
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 import serial
+import os
+os.system("spotify_client.py")
 
 ser = serial.Serial("/dev/ttyS0", baudrate = 9600, parity=serial.PARITY_NONE,
 stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 
 while 1:
     rx_data = ser.read() #read serial data
-    sleep(0.03)
+    sleep(0.03)    
     data_left = ser.inWaiting()
     rx_data += ser.data(data_left)
-    print(rx_data)
-    ser.write(rx_data) #write data to serial port
+    if rx_data:
+        get_current_song(sp)
+        toggle_playback(sp)
+    else:
+        continue
 
-#LCD control
-#LED control
-#Spotify API connection
-#Bluetooth connection
+#mbed controlling all physical components
