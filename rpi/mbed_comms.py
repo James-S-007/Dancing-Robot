e68ed01af8d3da33426a24926366dcd17bf9295b
James-S-007 <jspringer8@gatech.edu>
2022-04-19 22:39:24 -0400

    Cleanup, add check for empty song and fix uri bug

--- rpi/mbed_comms.py
+++ rpi/mbed_comms.py
@@ -4,12 +4,11 @@ import serial
 import os
 from spotify_client import SpotifyClient
 
-ser = serial.Serial("/dev/ttyACM0", baudrate=9600, parity=serial.PARITY_NONE,
-stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
+ser = serial.Serial("/dev/ttyACM0", baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 spotify_client = SpotifyClient()
-while 1:
-    # rx_data = ser.read() #read serial data
-    song_info = {'uri': None}
+
+while True:
+    curr_song_uri = None
     if ser.inWaiting():
         data = ser.readline()
         print(f'Data Received: {data}')
@@ -28,10 +27,12 @@ while 1:
         else:
             print('Err: Invalid character received')
     else:
-        # ser.write(str.encode('hello'))
-        new_song_info = spotify_client.get_current_song()
-        if new_song_info['uri'] != song_info['uri']:
-            song_info = new_song_info
+        song_info = spotify_client.get_current_song()
+        if not song_info:
+            song_info['name'] = 'No track playing'
+            song_info['artist'] = ''
+            song_info['bpm'] = 0
+        if song_info['uri'] != curr_song_uri:
             ser.write(str.encode(f"Song Name:{song_info['name']}\nArtist: {song_info['artist']}\nTempo: {song_info['tempo']}"))
-        sleep(1)
-    sleep(0.03)    
+        sleep(0.2)
+    sleep(0.03)
